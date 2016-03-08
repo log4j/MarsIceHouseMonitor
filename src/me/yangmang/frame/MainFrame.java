89ac0fbc5cac89e0258c8020f07a75e82d22736c
log4j <yangmang@msn.com>
2016-03-08 16:13:59 -0500

    commit

--- src/me/yangmang/frame/MainFrame.java
+++ src/me/yangmang/frame/MainFrame.java
@@ -152,13 +152,7 @@ public class MainFrame {
 		enableAllButton.setText("Enable All");
 		enableAllButton.setBounds(200, 20, 120, 30);
 		
-		enableAllButton.addMouseListener(
-				new MouseAdapter(){
-					@Override
-					public void mouseUp(MouseEvent arg0) {
-						facade.enableAllMonitor();
-					}
-				});
+		
 		
 		
 		
@@ -171,7 +165,7 @@ public class MainFrame {
 		disableAllButton.addMouseListener(
 				new MouseAdapter(){
 					@Override
-					public void mouseUp(MouseEvent arg0) {
+					public void mouseUp(MouseEvent e) {
 						facade.disableAllMonitor();
 					}
 				});
@@ -180,6 +174,22 @@ public class MainFrame {
 		clearAlarmButton.setText("Fix All");
 		clearAlarmButton.setBounds(200, 100, 120, 30);
 
+		
+
+		enableTemperatureAndHumidity = new Button(shell, SWT.PUSH);
+		enableTemperatureAndHumidity.setText("Enable Temp Humidity");
+		enableTemperatureAndHumidity.setBounds(200, 140, 160, 30);
+		
+		facade = new ControllerFacade();
+		
+		enableAllButton.addMouseListener(
+				new MouseAdapter(){
+					@Override
+					public void mouseUp(MouseEvent e) {
+						facade.enableAllMonitor();
+					}
+				});
+		
 		clearAlarmButton.addMouseListener(
 				new MouseAdapter(){
 					@Override
@@ -187,10 +197,6 @@ public class MainFrame {
 						facade.fixAllMonitor();
 					}
 				});
-
-		enableTemperatureAndHumidity = new Button(shell, SWT.PUSH);
-		enableTemperatureAndHumidity.setText("Enable Temp Humidity");
-		enableTemperatureAndHumidity.setBounds(200, 140, 160, 30);
 
 		enableTemperatureAndHumidity.addMouseListener(
 				new MouseAdapter(){
@@ -201,7 +207,7 @@ public class MainFrame {
 				});
 		
 
-		facade = new ControllerFacade();
+		
 		helper = new UpdateHelper(this);
 
 		 //this.updateData();
