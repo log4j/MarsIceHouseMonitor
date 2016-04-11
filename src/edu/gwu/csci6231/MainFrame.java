28eefb636805dff6a56eb4d1509e727b5c9d911f
Mang Yang <yangmang@msn.com>
2016-04-11 14:34:18 -0400

    mainframe change to csci6231

--- src/edu/gwu/csci6231/MainFrame.java
+++ src/edu/gwu/csci6231/MainFrame.java
@@ -9,6 +9,8 @@ import edu.gwu.csci6231.device.DeviceProvider;
 import edu.gwu.csci6231.device.SensorProvider;
 import edu.gwu.csci6231.device.model.DataModel;
 import edu.gwu.csci6231.frame.Console;
+import edu.gwu.csci6231.frame.ConsoleOperator;
+import edu.gwu.csci6231.frame.ConsoleTester;
 
 public class MainFrame {
 
@@ -22,20 +24,22 @@ public class MainFrame {
 
 		Shell shell = new Shell(display);
 		shell.setText("Mars Ice House 2");
-		shell.setSize(400, 300);
+		shell.setSize(600, 300);
 
 		this.display = display;
 		
 		shell.setLayout(new RowLayout(SWT.HORIZONTAL));
 		
-		Console console = new Console(shell,SWT.NONE);
+		Console consoleOperator = new ConsoleOperator(shell,SWT.NONE);
+		Console consoleTester = new ConsoleTester(shell,SWT.NONE);
 		
 		sensorProvider = new SensorProvider();
 		
 		
 		for(String name: sensorProvider.getDataModelNames()){
 			DataModel model = sensorProvider.getDataModel(name);
-			console.addDataModel(model);
+			consoleOperator.addDataModel(model);
+			consoleTester.addDataModel(model);
 		}
 		
 		
